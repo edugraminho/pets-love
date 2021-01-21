96eeff4806b0180d2ac28d84376cd916b8b27e8e
Eduardo Graminho <67005554+edugraminho@users.noreply.github.com>
2021-01-20 21:38:32 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 from os import environ
 
 
-application = create_app(environ.get('FLASK_ENV'))
+application = create_app("development")
 
 if __name__ == '__main__':
     application.run()
